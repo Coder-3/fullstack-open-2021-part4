5a63d48f9ef019d81c4b897f2e9fee78fa14fe8e
Cedric Quenette <c@cedricq.me>
2021-03-26 22:35:36 +1100

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blog) => {
   return 1
 }
 
+const totalLikes = (blogPosts) => {
+  let likes = 0
+
+  blogPosts.forEach(blog => {
+    likes += blog.likes
+  })
+
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
