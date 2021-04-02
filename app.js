83a4eb659b6dfaa43c144029172adc075b6809a7
Cedric Quenette <c@cedricq.me>
2021-04-03 09:31:22 +1100

    Creating users functionality implemented

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const config = require('./utils/config')
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -22,6 +23,7 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
